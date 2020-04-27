85fd39a932b3d1fe1a4ecea153d9b65d16a218a7
RifatBano <47645441+RifatBano@users.noreply.github.com>
2020-04-27 09:16:11 -0700

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/PaymentPayPalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentPayPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentPayPalApplication.class, args);
+	}
+
+}
